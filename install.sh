0867ff52ddc6475c33081edab40268ffceef1f12
François Schmidts <francois.schmidts@gmail.com>
2014-11-17 15:53:53 +0100

    adding output sugar and liquidprompt config

--- install.sh
+++ install.sh
@@ -2,16 +2,24 @@
 rm -rf ~/.vim ~/.bash_prompt
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 
+echo "### installing pathogen"
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+echo "### updating submodules"
 git submodule init
 git submodule update
 
-for f in cows vimrc bashrc bash_aliases profile gitconfig gitignore; do
-    rm -rf ~/.$f
-    ln -vs $PWD/$f ~/.$f
-done
-
+echo "### linking submodules"
 ln -vs $PWD/liquidprompt/liquidprompt ~/.bash_prompt
 ln -vs $PWD/syntastic ~/.vim/bundle/syntastic
-#ln -vs $PWD/ropevim ~/.vim/bundle/ropevim
+ln -vs $PWD/ropevim ~/.vim/bundle/ropevim
 
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+echo "### linking conf files"
+for f in cows vimrc bashrc bash_aliases profile gitconfig gitignore; do
+    rm -rvf ~/.$f
+    ln -vs $PWD/$f ~/.$f
+done
+rm -v ~/.config/liquidpromptrc
+ln -vs $PWD/liquidpromptrc ~/.config/liquidpromptrc
+
+
